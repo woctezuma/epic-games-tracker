f70d1c603f37694c30e315e180652c872ea23a72
Wok <woctezuma@users.noreply.github.com>
2022-09-11 22:57:59 +0200

    Fix bug: ensure a variable (data) is initialized

--- fetch_data_for_today.py
+++ fetch_data_for_today.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from src.data_utils import load_sandbox_ids_dict, populate_slugs
 from src.export_utils import write_markdown_files
 from src.fetch_utils import fetch_data_for_several_ids
-from src.json_utils import save_json
+from src.json_utils import save_json, load_json
 from src.time_utils import get_fname_for_today
 
 
@@ -15,6 +15,8 @@ def main():
         sandbox_ids_dict = load_sandbox_ids_dict()
         data = fetch_data_for_several_ids(sandbox_ids=sandbox_ids_dict.values())
         save_json(data, output_fname)
+    else:
+        data = load_json(output_fname)
 
     data = populate_slugs(data)
     write_markdown_files(data)
