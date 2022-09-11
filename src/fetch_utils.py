601f06fc7235e85fb96be1360c01029ab84d5e2f
Wok <woctezuma@users.noreply.github.com>
2022-09-11 20:19:27 +0200

    Add query wrapper to fetch data

---
+++ src/fetch_utils.py
@@ -0,0 +1,34 @@
+from src.query_achievement import to_achievement
+from src.query_game_rating import to_game_rating
+
+GAME_RATING_FIELDS = ['averageRating', 'ratingCount']
+ACHIEVEMENT_FIELDS = ['numProgressed', 'numCompleted']
+
+
+def fetch_data_for_single_id(sandbox_id):
+    data = dict()
+
+    game_rating = to_game_rating(sandbox_id)
+
+    for s in GAME_RATING_FIELDS:
+        data[s] = game_rating[s]
+
+    achievement = to_achievement(sandbox_id)
+    achievement_summary = achievement["achievementSets"][0]
+
+    for s in ACHIEVEMENT_FIELDS:
+        data[s] = achievement_summary[s]
+
+    return data
+
+
+def fetch_data_for_several_ids(sandbox_ids, verbose=True):
+    data = {}
+    num_sandbox_ids = len(sandbox_ids)
+
+    for i, sandbox_id in enumerate(sandbox_ids, start=1):
+        if verbose:
+            print(f"[{i}/{num_sandbox_ids}] {sandbox_id}")
+        data[sandbox_id] = fetch_data_for_single_id(sandbox_id)
+
+    return data
