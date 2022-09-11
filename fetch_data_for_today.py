1e23318d77f646391ca279966d6e791dbb01a934
Wok <woctezuma@users.noreply.github.com>
2022-09-11 22:38:02 +0200

    Write Markdown files at the end of the daily script

--- fetch_data_for_today.py
+++ fetch_data_for_today.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from src.data_utils import load_sandbox_ids_dict
+from src.data_utils import load_sandbox_ids_dict, populate_slugs
+from src.export_utils import write_markdown_files
 from src.fetch_utils import fetch_data_for_several_ids
 from src.json_utils import save_json
 from src.time_utils import get_fname_for_today
@@ -14,6 +15,9 @@ def main():
         sandbox_ids_dict = load_sandbox_ids_dict()
         data = fetch_data_for_several_ids(sandbox_ids=sandbox_ids_dict.values())
         save_json(data, output_fname)
+
+    data = populate_slugs(data)
+    write_markdown_files(data)
 
     return
 
