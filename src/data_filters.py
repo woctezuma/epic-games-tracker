9ed3e7a46be6a41b7f445c1ef9abe307bba46345
Wok <woctezuma@users.noreply.github.com>
2022-09-12 21:36:34 +0200

    Minor refactoring: clarify code for None elements

--- src/data_filters.py
+++ src/data_filters.py
@@ -1,2 +1,6 @@
+def has_at_lease_one_none_element(v):
+    return any(e is None for e in v.values())
+
+
 def hide_none_elements(data):
-    return {k: v for k, v in data.items() if all(e is not None for e in v.values())}
+    return {k: v for k, v in data.items() if not has_at_lease_one_none_element(v)}
