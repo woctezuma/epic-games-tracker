10dab72c457cd8420e082831c2b1113828e43eea
Wok <woctezuma@users.noreply.github.com>
2022-09-12 21:37:43 +0200

    Minor refactoring: rename function to hide games

--- src/data_filters.py
+++ src/data_filters.py
@@ -2,5 +2,5 @@ def has_at_lease_one_none_element(v):
     return any(e is None for e in v.values())
 
 
-def hide_none_elements(data):
+def hide_games_with_none_elements(data):
     return {k: v for k, v in data.items() if not has_at_lease_one_none_element(v)}
