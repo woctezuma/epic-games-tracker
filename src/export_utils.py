0f6eda528b9bc3912190e7391873df4e714c9f94
Wok <woctezuma@users.noreply.github.com>
2022-09-12 19:31:45 +0200

    Deal with None when sorting numeric values

--- src/export_utils.py
+++ src/export_utils.py
@@ -16,14 +16,21 @@ def write_data_to_disk(data, fname):
     return
 
 
+def deal_with_none(v, min_value=0):
+    # Reference: https://stackoverflow.com/a/12971697/376454
+    if not v:
+        return min_value
+    return v
+
+
 def write_markdown_files(data):
     sorted_data = dict(sorted(data.items(), key=lambda x: x[1]["slug"]))
     write_data_to_disk(sorted_data, f"{OUTPUT_FOLDER}/by_game_slug.md")
 
-    sorted_data = dict(sorted(data.items(), key=lambda x: x[1]["averageRating"], reverse=True))
+    sorted_data = dict(sorted(data.items(), key=lambda x: deal_with_none(x[1]["averageRating"]), reverse=True))
     write_data_to_disk(sorted_data, f"{OUTPUT_FOLDER}/by_average_rating.md")
 
-    sorted_data = dict(sorted(data.items(), key=lambda x: x[1]["ratingCount"], reverse=True))
+    sorted_data = dict(sorted(data.items(), key=lambda x: deal_with_none(x[1]["ratingCount"]), reverse=True))
     write_data_to_disk(sorted_data, f"{OUTPUT_FOLDER}/by_num_raters.md")
 
     sorted_data = dict(sorted(data.items(), key=lambda x: x[1]["numProgressed"], reverse=True))
