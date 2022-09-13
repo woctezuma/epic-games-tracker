6031191c40f9b55f63e953ee8bfc01dc003748c9
Wok <woctezuma@users.noreply.github.com>
2022-09-13 22:00:15 +0200

    Add utils to extract the highest unlock percentage of achievements

---
+++ src/achievement_utils.py
@@ -0,0 +1,20 @@
+def list_all_unlock_percentages(achievement_data):
+    return [e['achievement']['rarity']['percent'] for e in achievement_data['achievements']]
+
+
+def compute_max_unlock_percentage(achievement_data):
+    rarity_list = list_all_unlock_percentages(achievement_data)
+
+    if len(rarity_list) > 0:
+        rarity = max(rarity_list)
+    else:
+        rarity = None
+
+    return rarity
+
+
+def summarize_achievement(achievement_data):
+    achievement_summary = achievement_data["achievementSets"][0]
+    achievement_summary['maxRarity'] = compute_max_unlock_percentage(achievement_data)
+
+    return achievement_summary
