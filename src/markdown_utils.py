f650c8744e74c3b82d84631b9a2a86d38af62195
Wok <woctezuma@users.noreply.github.com>
2022-09-13 23:05:08 +0200

    Create row for table headers

--- src/markdown_utils.py
+++ src/markdown_utils.py
@@ -1,7 +1,9 @@
 from src.fetch_utils import GAME_RATING_FIELDS, ACHIEVEMENT_FIELDS
 from src.time_utils import get_current_date_as_str
 
-HEADERS = "| # | Game Slug | Average Rating | Number of Raters | Number of Players | Number of Platinum Trophies | Max Rarity (%) |"
+GAME_RATING_HEADERS = ["Average Rating", "Number of Raters"]
+ACHIEVEMENT_HEADERS = ["Number of Players", "Number of Platinum Trophies", "Max Rarity (%)"]
+HEADERS = ["Game Slug"] + GAME_RATING_HEADERS + ACHIEVEMENT_HEADERS
 TABLE_SEPARATOR = "|---|---|---|---|---|---|---|"
 ENTRY_FIELDS = ['slug'] + GAME_RATING_FIELDS + ACHIEVEMENT_FIELDS
 
@@ -17,8 +19,13 @@ def to_table_row(row_no, str_elements):
     return line
 
 
+def get_headers_line():
+    place_holder_for_number = '#'
+    return to_table_row(place_holder_for_number, HEADERS)
+
+
 def format_data_as_markdown(data):
-    lines = [get_timestamp_line(), "\n", HEADERS, TABLE_SEPARATOR]
+    lines = [get_timestamp_line(), "\n", get_headers_line(), TABLE_SEPARATOR]
 
     for i, entry in enumerate(data.values(), start=1):
         line = to_table_row(i, [str(entry[k]) for k in ENTRY_FIELDS])
