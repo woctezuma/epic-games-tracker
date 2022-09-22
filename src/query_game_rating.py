727ca916ffe95c5b0afd3155339622c25fe3ee25
Wok <woctezuma@users.noreply.github.com>
2022-09-22 18:51:25 +0200

    Query game ratings using GraphQL without sha256Hash

--- src/query_game_rating.py
+++ src/query_game_rating.py
@@ -2,16 +2,13 @@ from src.api import send_post_request_to_api
 
 
 def get_params_to_query_game_rating(sandbox_id):
-    params = {
-        "operationName": "getProductResult",
-        "variables": {"sandboxId": sandbox_id, "locale": "en"},
-        "extensions": {
-            "persistedQuery": {
-                "version": 1,
-                "sha256Hash": "b6795ca724814d54cfc1854698773d1ee7b0721b326f64fc08b48a3b9358b01d",
-            }
-        },
-    }
+    query_str = "{RatingsPolls {getProductResult"
+    query_str += f'(sandboxId: {sandbox_id}, locale: "en") '
+    query_str += "{"
+    query_str += "averageRating ratingCount"
+    query_str += "}}}"
+
+    params = {"query": query_str}
 
     return params
 
