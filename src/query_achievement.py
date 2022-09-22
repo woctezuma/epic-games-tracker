394234b3a5363db064dbd9b602c92923baa2315f
Wok <woctezuma@users.noreply.github.com>
2022-09-22 18:51:38 +0200

    Query achievements using GraphQL without sha256Hash

--- src/query_achievement.py
+++ src/query_achievement.py
@@ -2,16 +2,13 @@ from src.api import send_post_request_to_api
 
 
 def get_params_to_query_achievement(sandbox_id):
-    params = {
-        "operationName": "Achievement",
-        "variables": {"sandboxId": sandbox_id, "locale": "en"},
-        "extensions": {
-            "persistedQuery": {
-                "version": 1,
-                "sha256Hash": "7d54399ad8b8b5538bc2d93ee66b07014432b5488945cda35fe0b1fc70eea83a",
-            }
-        },
-    }
+    query_str = "{Achievement {productAchievementsRecordBySandbox"
+    query_str += f'(sandboxId: {sandbox_id}, locale: "en") '
+    query_str += "{"
+    query_str += "achievementSets {numProgressed numCompleted} achievements {achievement {rarity {percent} } }"
+    query_str += "}}}"
+
+    params = {"query": query_str}
 
     return params
 
