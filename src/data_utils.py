0036b66562df3f6287974760c4006c8e6e49441e
Wok <woctezuma@users.noreply.github.com>
2022-09-27 22:27:39 +0200

    Allow to load a file with ALL page slugs and sandbox IDs

--- src/data_utils.py
+++ src/data_utils.py
@@ -3,10 +3,20 @@ from src.json_utils import load_json
 
 DATA_FOLDER_NAME = 'data'
 SANDBOX_IDS_FNAME = 'sandbox_ids.json'
+ALL_SANDBOX_IDS_FNAME = 'sandbox_ids_all.json'
 
 
 def load_sandbox_ids_dict():
     return load_json(f"{DATA_FOLDER_NAME}/{SANDBOX_IDS_FNAME}")
+
+
+def load_all_sandbox_ids_dict():
+    try:
+        data = load_json(f"{DATA_FOLDER_NAME}/{ALL_SANDBOX_IDS_FNAME}")
+    except FileNotFoundError:
+        data = {}
+
+    return data
 
 
 def populate_slugs(data):
