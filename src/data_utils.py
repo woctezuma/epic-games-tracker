b2ecea61743c3a2d3516f21a1504c2dbb9d6bfd4
Wok <woctezuma@users.noreply.github.com>
2022-09-27 23:18:00 +0200

    Minor refactoring: move folder name into file name constants

--- src/data_utils.py
+++ src/data_utils.py
@@ -2,17 +2,17 @@ from src.game_rating_utils import compute_rating_count
 from src.json_utils import load_json
 
 DATA_FOLDER_NAME = 'data'
-SANDBOX_IDS_FNAME = 'sandbox_ids.json'
-PAGE_MAPPINGS_FNAME = 'page_mappings.json'
+SANDBOX_IDS_FNAME = f'{DATA_FOLDER_NAME}/sandbox_ids.json'
+PAGE_MAPPINGS_FNAME = f'{DATA_FOLDER_NAME}/page_mappings.json'
 
 
 def load_tracked_page_mappings():
-    return load_json(f"{DATA_FOLDER_NAME}/{SANDBOX_IDS_FNAME}")
+    return load_json(f"{SANDBOX_IDS_FNAME}")
 
 
 def load_all_page_mappings():
     try:
-        data = load_json(f"{DATA_FOLDER_NAME}/{PAGE_MAPPINGS_FNAME}")
+        data = load_json(f"{PAGE_MAPPINGS_FNAME}")
     except FileNotFoundError:
         data = {}
 
