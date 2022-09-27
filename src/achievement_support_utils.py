d702b01c5c913ea86e5673226b9b3318074fa3b3
Wok <woctezuma@users.noreply.github.com>
2022-09-28 00:39:53 +0200

    Add utils to parse achievement support (total achievements)

---
+++ src/achievement_support_utils.py
@@ -0,0 +1,11 @@
+def get_num_achievements(achievement_data):
+    try:
+        num_achievements = achievement_data["totalAchievements"]
+    except (TypeError, KeyError) as e:
+        num_achievements = None
+    return num_achievements
+
+
+def supports_achievements(achievement_data):
+    num_achievements = get_num_achievements(achievement_data)
+    return bool(num_achievements is not None)
