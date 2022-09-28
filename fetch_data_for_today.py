ef9a542c2ce2ecdc633cda483b079941cd47715a
Wok <woctezuma@users.noreply.github.com>
2022-09-28 12:42:00 +0200

    Change default indent value (2->4)

--- fetch_data_for_today.py
+++ fetch_data_for_today.py
@@ -21,7 +21,7 @@ def main():
         print('Updating JSON.')
         sandbox_ids_dict = load_tracked_page_mappings()
         data = fetch_data_for_several_ids(sandbox_ids=sandbox_ids_dict.values())
-        save_json(data, output_fname, prettify=True)
+        save_json(data, output_fname, prettify=True, indent=2)
     else:
         data = load_json(output_fname)
 
--- src/json_utils.py
+++ src/json_utils.py
@@ -7,7 +7,7 @@ def load_json(fname):
     return data
 
 
-def save_json(data, fname, prettify=False, indent=2):
+def save_json(data, fname, prettify=False, indent=4):
     with open(fname, 'w', encoding='utf8') as f:
         if prettify:
             json.dump(data, f, indent=indent)
