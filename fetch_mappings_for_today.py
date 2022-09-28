3acbed4f8cc9849ee41388eb0698d77a22bf71ac
Wok <woctezuma@users.noreply.github.com>
2022-09-28 11:43:31 +0200

    Minor refactoring: move function to filter mappings with slugs

--- fetch_mappings_for_today.py
+++ fetch_mappings_for_today.py
@@ -5,6 +5,7 @@ from src.data_utils import load_tracked_page_mappings, load_all_page_mappings
 from src.download_utils import download_page_slugs, download_page_mappings
 from src.download_utils import filter_page_mappings_based_on_achievement_support
 from src.json_utils import save_json, load_json
+from src.page_mapping_utils import filter_page_mappings_based_on_slugs
 from src.utils import sort_dict_by_key
 
 
@@ -20,7 +21,7 @@ def main():
     page_mappings = download_page_mappings(page_slugs, known_page_mappings=load_all_page_mappings())
     save_json(sort_dict_by_key(page_mappings), PAGE_MAPPINGS_FNAME, prettify=True)
 
-    page_mappings = {k: v for k, v in page_mappings.items() if k in page_slugs}
+    page_mappings = filter_page_mappings_based_on_slugs(page_mappings, page_slugs)
     sandbox_ids_dict = filter_page_mappings_based_on_achievement_support(page_mappings,
                                                                          known_support=load_tracked_page_mappings())
     save_json(sort_dict_by_key(sandbox_ids_dict), SANDBOX_IDS_FNAME, prettify=True)
--- src/page_mapping_utils.py
+++ src/page_mapping_utils.py
@@ -1,2 +1,6 @@
 def get_sandbox_id(mapping_data):
     return mapping_data["sandboxId"]
+
+
+def filter_page_mappings_based_on_slugs(page_mappings, page_slugs):
+    return {k: v for k, v in page_mappings.items() if k in page_slugs}
