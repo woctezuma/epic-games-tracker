f94760d12f15bcbff1e5cf9b16dc3cbf0e054518
Wok <woctezuma@users.noreply.github.com>
2022-10-01 19:10:43 +0200

    Increase verbosity when updating tracked IDs

--- fetch_mappings_for_today.py
+++ fetch_mappings_for_today.py
@@ -13,14 +13,17 @@ def main():
     force_update = True
 
     if force_update or not Path(PAGE_SLUGS_FNAME).exists():
+        print('Updating page slugs.')
         page_slugs = download_page_slugs(include_dlc=False)
         save_json(sorted(page_slugs), PAGE_SLUGS_FNAME, prettify=True)
     else:
         page_slugs = load_json(PAGE_SLUGS_FNAME)
 
+    print('Updating all page mappings.')
     page_mappings = download_page_mappings(page_slugs, known_page_mappings=load_all_page_mappings())
     save_json(sort_dict_by_key(page_mappings), PAGE_MAPPINGS_FNAME, prettify=True)
 
+    print('Updating tracked page mappings.')
     page_mappings_of_interest = filter_page_mappings_based_on_slugs(page_mappings, page_slugs)
     sandbox_ids_dict = download_achievement_support_to_filter_page_mappings(page_mappings_of_interest,
                                                                             known_support=load_tracked_page_mappings())
