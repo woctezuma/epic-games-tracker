6407e107bd25ad515109b7cbe89fc6c55c43c5bd
Wok <woctezuma@users.noreply.github.com>
2022-12-16 22:12:07 +0100

    Add utils to parse updated date

--- src/page_mapping_utils.py
+++ src/page_mapping_utils.py
@@ -3,5 +3,9 @@ import operator
 get_sandbox_id = operator.itemgetter('sandboxId')
 
 
+def get_updated_date(mapping_data):
+    return mapping_data["updatedDate"]
+
+
 def filter_page_mappings_based_on_slugs(page_mappings, page_slugs):
     return {k: v for k, v in page_mappings.items() if k in page_slugs}
