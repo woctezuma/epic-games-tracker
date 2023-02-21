91fd7eb798373e52d4d68cc4dba4437f2a56cac9
Wok <woctezuma@users.noreply.github.com>
2023-02-21 15:53:59 +0100

    Find the filename for yesterday

--- src/time_utils.py
+++ src/time_utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from src.data_utils import DATA_FOLDER_NAME
 
@@ -19,6 +19,10 @@ def get_folder_name_for_specific_day(date):
 def get_fname_for_specific_day(date):
     folder_name = get_folder_name_for_specific_day(date)
     return f"{folder_name}/{date.day:02}.json"
+
+def get_fname_for_yesterday():
+    date = get_current_date() - timedelta(days=1)
+    return get_fname_for_specific_day(date)
 
 
 def get_fname_for_today():
