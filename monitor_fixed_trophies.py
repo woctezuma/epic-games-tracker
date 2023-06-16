eb7b2f7399aecbedc30e3af557e6ddb024d6b50c
Wok <woctezuma@users.noreply.github.com>
2023-06-17 00:01:50 +0200

    Allow to load previous data without assuming a daily fetching

--- monitor_fixed_trophies.py
+++ monitor_fixed_trophies.py
@@ -1,12 +1,15 @@
 from src.achievement_fixes import list_slugs_with_fixed_achievements
 from src.discord_utils import post_slugs_to_discord
 from src.json_utils import load_json, load_json_failsafe
-from src.time_utils import get_fname_for_today, get_fname_for_yesterday
+from src.time_utils import get_fname_for_today, get_fname_for_yesterday, get_fname_for_the_most_recent_of_past_days
 from src.webhook_utils import WEBHOOK_KEYWORD_FIXED_TROPHY
 
 
 def main():
-    data_yesterday = load_json_failsafe(fname=get_fname_for_yesterday())
+    try:
+        data_yesterday = load_json(fname=get_fname_for_yesterday())
+    except FileNotFoundError:
+        data_yesterday = load_json_failsafe(fname=get_fname_for_the_most_recent_of_past_days())
     data_today = load_json(fname=get_fname_for_today())
 
     game_slugs = list_slugs_with_fixed_achievements(data_yesterday, data_today)
--- src/time_utils.py
+++ src/time_utils.py
@@ -1,6 +1,9 @@
+import glob
 from datetime import datetime, timedelta
 
 from src.data_utils import DATA_FOLDER_NAME
+
+DATA_FILE_NAME_FORMAT = f'{DATA_FOLDER_NAME}/20*/*/*.json'
 
 
 def get_current_date():
@@ -28,3 +31,13 @@ def get_fname_for_yesterday():
 def get_fname_for_today():
     date = get_current_date()
     return get_fname_for_specific_day(date)
+
+
+def list_data_file_names():
+    return sorted(glob.glob(DATA_FILE_NAME_FORMAT))
+
+
+def get_fname_for_the_most_recent_of_past_days():
+    all_fnames = list_data_file_names()
+    fnames_for_past_days = all_fnames[:-1]
+    return fnames_for_past_days[-1]
