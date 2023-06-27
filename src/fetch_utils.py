30351146f38ae0d2465eb9bb7410445a5fbb06a8
Wok <woctezuma@users.noreply.github.com>
2023-06-27 11:16:52 +0200

    Fetch achievements and ratings in a single request

--- src/fetch_utils.py
+++ src/fetch_utils.py
@@ -3,6 +3,7 @@ import time
 from src.achievement_utils import summarize_achievement
 from src.fields import RAW_RATING_FIELDS, ACHIEVEMENT_FIELDS
 from src.query_achievement import to_achievement
+from src.query_game_data import to_game_data
 from src.query_game_rating import to_game_rating
 from src.retry_utils import is_buggy_achievement_data, is_buggy_game_rating_data
 
@@ -12,7 +13,7 @@ COOLDOWN_DURATION_IN_SECONDS = 2
 def fetch_data_for_single_id(sandbox_id):
     data = dict()
 
-    game_rating = to_game_rating(sandbox_id)
+    achievement, game_rating = to_game_data(sandbox_id)
 
     if is_buggy_game_rating_data(game_rating):
         print(f'Try again {sandbox_id} in {COOLDOWN_DURATION_IN_SECONDS}s.')
@@ -24,8 +25,6 @@ def fetch_data_for_single_id(sandbox_id):
             data[s] = game_rating[s]
         except TypeError:
             data[s] = None
-
-    achievement = to_achievement(sandbox_id)
 
     if is_buggy_achievement_data(achievement):
         print(f'Try again {sandbox_id} in {COOLDOWN_DURATION_IN_SECONDS}s.')
