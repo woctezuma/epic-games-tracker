48d957ec7080d88221b66ca649cb9cb75850824c
Wok <woctezuma@users.noreply.github.com>
2023-06-27 10:27:06 +0200

    Minor refactoring: rename and move function to extract list difference

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,6 @@
 def sort_dict_by_key(d):
     return dict(sorted(d.items(), key=lambda x: x[0]))
+
+
+def extract_list_difference(new_list, old_list):
+    return list(set(new_list).difference(old_list))
