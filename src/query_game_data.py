0a9f5ffa28c7282c0559a07f62f3aa69811be69f
Wok <woctezuma@users.noreply.github.com>
2023-06-27 11:15:19 +0200

    Allow to query achievements and ratings in a single request

---
+++ src/query_game_data.py
@@ -0,0 +1,34 @@
+from src.api import send_post_request_to_api
+from src.query_utils import get_query_str_for_achievements, get_query_str_for_ratings
+
+
+def get_params_to_query_game_data(sandbox_id):
+    query_str = "{"
+    query_str += get_query_str_for_achievements(sandbox_id)
+    query_str += get_query_str_for_ratings(sandbox_id)
+    query_str += "}"
+
+    params = {"query": query_str}
+
+    return params
+
+
+def to_game_data(sandbox_id, verbose=True):
+    params = get_params_to_query_game_data(sandbox_id)
+    data = send_post_request_to_api(params, verbose=verbose)
+
+    try:
+        achievement = data["data"]["Achievement"]["productAchievementsRecordBySandbox"]
+    except KeyError as e:
+        achievement = {}
+    except TypeError as e:
+        achievement = None
+
+    try:
+        game_rating = data["data"]["RatingsPolls"]["getProductResult"]
+    except KeyError as e:
+        game_rating = {}
+    except TypeError as e:
+        game_rating = None
+
+    return achievement, game_rating
