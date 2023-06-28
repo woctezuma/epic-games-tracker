d39544119606ca63ffcc1e2394c1e90eceb112ec
Wok <woctezuma@users.noreply.github.com>
2023-06-28 22:42:10 +0200

    Remove manual retries, unnecessary thanks to the "backoff" package

--- src/fetch_utils.py
+++ src/fetch_utils.py
@@ -1,14 +1,8 @@
-import time
-
 from src.achievement_utils import summarize_achievement
 from src.fields import RAW_RATING_FIELDS, ACHIEVEMENT_FIELDS
-from src.query_achievement import to_achievement
 from src.query_game_data import to_game_data
-from src.query_game_rating import to_game_rating
 from src.retry_utils import is_buggy_achievement_data, is_buggy_game_rating_data
 from src.utils import create_dummy_dictionary
-
-COOLDOWN_DURATION_IN_SECONDS = 2
 
 
 def fetch_data_for_single_id(sandbox_id):
@@ -17,33 +11,19 @@ def fetch_data_for_single_id(sandbox_id):
     achievement, game_rating = to_game_data(sandbox_id)
 
     if is_buggy_achievement_data(achievement):
-        print(f'[achievement] Try again {sandbox_id} in {COOLDOWN_DURATION_IN_SECONDS}s.')
-        time.sleep(COOLDOWN_DURATION_IN_SECONDS)
-        achievement = to_achievement(sandbox_id)
-        if not is_buggy_achievement_data(achievement):
-            print(f'[SUCCESS] achievement data has been found for {sandbox_id}.')
-
-    try:
-        achievement_summary = summarize_achievement(achievement)
-    except IndexError:
         print(f'[ERROR] achievement data cannot be found for {sandbox_id}.')
         achievement_summary = create_dummy_dictionary(ACHIEVEMENT_FIELDS)
+    else:
+        achievement_summary = summarize_achievement(achievement)
 
     for s in ACHIEVEMENT_FIELDS:
         data[s] = achievement_summary[s]
 
     if is_buggy_game_rating_data(game_rating):
-        print(f'[rating] Try again {sandbox_id} in {COOLDOWN_DURATION_IN_SECONDS}s.')
-        time.sleep(COOLDOWN_DURATION_IN_SECONDS)
-        game_rating = to_game_rating(sandbox_id)
-        if not is_buggy_game_rating_data(game_rating):
-            print(f'[SUCCESS] rating data has been found for {sandbox_id}.')
+        game_rating = create_dummy_dictionary(RAW_RATING_FIELDS)
 
     for s in RAW_RATING_FIELDS:
-        try:
-            data[s] = game_rating[s]
-        except TypeError as e:
-            data[s] = None
+        data[s] = game_rating[s]
 
     return data
 
