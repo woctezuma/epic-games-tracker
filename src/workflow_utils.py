2d43e4d8245a977a50b7000343ad977ceac10dd3
Wok <woctezuma@users.noreply.github.com>
2023-06-28 11:06:04 +0200

    Refactoring: move workflows into functions

---
+++ src/workflow_utils.py
@@ -0,0 +1,33 @@
+from src.data_utils import load_all_page_mappings, load_tracked_page_mappings, SANDBOX_IDS_FNAME, PAGE_MAPPINGS_FNAME
+from src.download_utils import download_achievement_support_to_filter_page_mappings, download_page_mappings
+from src.json_utils import save_json
+from src.page_mapping_utils import filter_page_mappings_based_on_slugs
+from src.utils import sort_dict_by_key, extract_list_difference
+
+
+def update_all_page_mappings(page_slugs, known_page_mappings=None):
+    if known_page_mappings is None:
+        known_page_mappings = load_all_page_mappings()
+
+    page_mappings = download_page_mappings(page_slugs, known_page_mappings=known_page_mappings)
+    save_json(sort_dict_by_key(page_mappings), PAGE_MAPPINGS_FNAME, prettify=True)
+
+    new_game_slugs = extract_list_difference(page_mappings, known_page_mappings)
+
+    return new_game_slugs
+
+
+def update_tracked_page_mappings(page_slugs, known_page_mappings=None):
+    if known_page_mappings is None:
+        known_page_mappings = load_all_page_mappings()
+
+    page_mappings_of_interest = filter_page_mappings_based_on_slugs(known_page_mappings, page_slugs)
+
+    known_support = load_tracked_page_mappings()
+    sandbox_ids_dict = download_achievement_support_to_filter_page_mappings(page_mappings_of_interest,
+                                                                            known_support=known_support)
+    save_json(sort_dict_by_key(sandbox_ids_dict), SANDBOX_IDS_FNAME, prettify=True)
+
+    new_tracked_game_slugs = extract_list_difference(sandbox_ids_dict, known_support)
+
+    return new_tracked_game_slugs
