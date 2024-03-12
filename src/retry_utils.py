e2774ce02f1ff3d0d93b6a3e9185d6459d9df598
Wok <woctezuma@users.noreply.github.com>
2024-03-12 08:56:52 +0100

    Fix a bug which results from the catch of JSONDecodeError

--- src/retry_utils.py
+++ src/retry_utils.py
@@ -7,7 +7,11 @@ def has_buggy_achievements(achievement):
 
 
 def has_no_achievement(achievement):
-    return has_buggy_achievement_sets(achievement) or has_buggy_achievements(achievement)
+    return (
+        achievement is None
+        or has_buggy_achievement_sets(achievement)
+        or has_buggy_achievements(achievement)
+    )
 
 
 def has_no_rating(game_rating):
