88785ecfce67c250b0d3cf9a3faf58e6df796da7
Wok <woctezuma@users.noreply.github.com>
2025-07-19 14:45:37 +0200

    Rely on cloudscraper

--- src/api.py
+++ src/api.py
@@ -1,18 +1,21 @@
 import backoff
-import requests
+import cloudscraper
 
 TIMEOUT_IN_SECONDS = 10
 GRAPHQL_API_URL = "https://store.epicgames.com/graphql"
 
 
 def send_post_request_to_api(json_data, verbose=True):
-    r = to_response(json_data)
+    scraper = cloudscraper.create_scraper()
+    r = to_response(json_data, scraper=scraper)
     return to_data(r, verbose=verbose)
 
 
 @backoff.on_predicate(backoff.expo, lambda r: not r.ok, max_tries=3)
-def to_response(json_data):
-    return requests.post(GRAPHQL_API_URL, json=json_data, timeout=TIMEOUT_IN_SECONDS)
+def to_response(json_data, scraper: cloudscraper.CloudScraper | None = None):
+    if scraper is None:
+        scraper = cloudscraper.create_scraper()
+    return scraper.post(GRAPHQL_API_URL, json=json_data, timeout=TIMEOUT_IN_SECONDS)
 
 
 def to_data(response, verbose=True):
